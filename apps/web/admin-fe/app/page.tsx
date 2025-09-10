22403a7c46fe9d89bb06d0758c4082817b65cd12
Abhas Behera <abhasbehera560@gmail.com>
2025-09-10 14:57:10 +0530

    fix

--- apps/web/admin-fe/app/page.tsx
+++ apps/web/admin-fe/app/page.tsx
@@ -1,10 +1,9 @@
-import { Label } from "@/components/ui/label";
-
+import AdminSignIn from "./auth/page";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Label className="text-2xl font-bold">Welcome to Swaraj</Label>
+    <div>
+      <AdminSignIn />
     </div>
   );
 }
